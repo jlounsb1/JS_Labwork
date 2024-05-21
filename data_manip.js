b6afdb01de0b7f976dbb74bb1098f2a53bd80321
jlounsb1 <jefflounsbury@gmail.com>
2024-05-21 13:52:02 -0400

    Exercise 2 finished.

--- data_manip.js
+++ data_manip.js
@@ -49,24 +49,75 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
 //Exercise 1
 const isDivisible5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) ==0;
-console.log(isDivisible5);
+console.log(`Are the Numbers divisible by 5: ${isDivisible5}`);
 //Checking to see if the numbers are divisible by five with modulus. If there was a remainder this would be false
 
 const isGreater = n1>n4;
-console.log(isGreater);
+console.log(`Is n1 greater than n4: ${isGreater}`);
 //checking if n1 is more than n4
 
 const arithChain = (((n2-n1)*n3)%n4);
-console.log(arithChain);
+console.log(`What is the result of (((n2-n1)*n3)Modulus n4): ${arithChain}`);
 //performing arithmetic needed from exercise
 
 const newUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
-console.log(newUnder25);
+console.log(`Are all the numbers less than 25: ${newUnder25}`);
 const newIsValid = isSum50 && isTwoOdd && newUnder25 && isUnique;
-console.log(newIsValid);
+console.log(`Are all my calculations true, therefore is my code valid: ${newIsValid}`);
 //changed the greater than to less than and the operation to AND
 //if any number is greater than 25, the variable newOver25 would return false
 //then switched the isValid to a new variable with ! removed
 
 
 //Exercise 2
+
+let totalDistance = 1500;
+let fuelBudget = 175;
+let costPerGallon =3;
+let fuelEff55 = 30;
+let fuelEff60 = 28;
+let fuelEff75 = 23;
+//variables declared for road trip exercise
+
+//calculate each question travelling at 55, 60, and 75
+
+//How many gallons of fuel will you need for the entire trip?
+
+let totalFuel55 = Math.round(totalDistance / fuelEff55);
+console.log(`Gallons needed going 55mph: ${totalFuel55}`);
+
+let totalFuel60 = Math.round(totalDistance / fuelEff60);
+console.log(`Gallons needed going 60mph: ${totalFuel60}`);
+
+let totalFuel75 = Math.round(totalDistance / fuelEff75);
+console.log(`Gallons needed going 75mph: ${totalFuel75}`);
+
+//Will your budget be enough to cover the fuel expesnse?
+
+let tripCost55 = Math.round(costPerGallon * totalFuel55);
+let budgetPass55 = tripCost55 < fuelBudget;
+console.log(`Fuel cost of the trip travelling at 55mph: ${tripCost55}. Do I have the budget:${budgetPass55}`);
+
+let tripCost60 = Math.round(costPerGallon * totalFuel60);
+let budgetPass60 = tripCost60 < fuelBudget;
+console.log(`Fuel cost of the trip travelling at 60mph: ${tripCost60}. Do I have the budget:${budgetPass60}`);
+
+let tripCost75 = Math.round(costPerGallon * totalFuel75);
+let budgetPass75 = tripCost75 < fuelBudget;
+console.log(`Fuel cost of the trip travelling at 75mph: ${tripCost75}. Do I have the budget:${budgetPass75}`);
+
+//The output of these calculations show you cannot travel at 75 and make the trip under budget.
+
+//How long will the trip take in hours?
+
+let timeSpeed55 = Math.round(totalDistance /55);
+console.log(`Time to destination going 55mph : ${timeSpeed55} hours.`);
+
+let timeSpeed60 = Math.round(totalDistance /60);
+console.log(`Time to destination going 60mph : ${timeSpeed60} hours.`);
+
+let timeSpeed75 = Math.round(totalDistance /75);
+console.log(`Time to destination going 75mph : ${timeSpeed75} hours.`);
+
+console.log('It makes the most sense to go on the trip at 60 mph. This will allow you to make the best time, but also keep in budget.')
+
